7b933f947c50776933c4398ff3185eba6e56d5ba
thedevilx <aadithya794@gmail.com>
2020-09-25 02:45:51 +0530

    Added Maps

--- src/pages/maps/Maps.js
+++ src/pages/maps/Maps.js
@@ -28,12 +28,13 @@ export default function Maps() {
 
   return (
     <div className={classes.mapContainer}>
-      <BasicMap
+      {/* <BasicMap
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8mzJXPad-RJbAandipO8DcBxN2EsRn7c&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: "inherit", width: "inherit" }} />}
         containerElement={<div style={{ height: "100%" }} />}
         mapElement={<div style={{ height: "100%" }} />}
-      />
+      /> */}
+      <iframe alt="jugaad" title="slum-maps-lifesaver" src="https://www.google.com/maps/d/u/0/embed?mid=1gJ-arD9spvrIG2oVQaPILlSUZsqn_jK1" width="100%" height="100%"></iframe>
     </div>
   );
 }
